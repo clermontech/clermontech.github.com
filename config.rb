fc84c0eca282f555f1052d54fcb62b6840abf7ff
William Durand <will+git@drnd.me>
2015-03-01 02:50:47 +0100

    update css

--- config.rb
+++ config.rb
@@ -12,14 +12,16 @@ javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = true
+line_comments = false
 
-sass_options = { :sourcemap => true }
+sass_options = { :sourcemap => false }
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
